917539a48894617c31019fbf9c026b4a31dde8ac
tipsypastels <dakotasankey@gmail.com>
2018-12-04 14:59:55 -0800

    added super basic express routing

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log('hello world');
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.json({ message: 'hello world!' });
+});
+
+app.use('/api', router);
+app.listen(port);
+console.log(`Listening on PORT: ${port}!`);
