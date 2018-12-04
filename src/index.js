6fbb3374bfcb27868dca27a6116b3f3803ce84ea
tipsypastels <dakotasankey@gmail.com>
2018-12-04 15:02:24 -0800

    comments + /v1 route

--- src/index.js
+++ src/index.js
@@ -1,18 +1,25 @@
+// ES6 syntax for require()
 import express from 'express';
 import bodyParser from 'body-parser';
 
+// server setup
 const app = express();
 const port = process.env.PORT || 8080;
 
+// app.use allows us to add middleware,
+// in this case bodyParser which lets us handle POST requests
+// that pass additional data
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+// create a router do define the endpoints
 const router = express.Router();
 
 router.get('/', (req, res) => {
   res.json({ message: 'hello world!' });
 });
 
-app.use('/api', router);
+// prefixes /api to all our routes, good practice
+app.use('/api/v1/', router);
 app.listen(port);
-console.log(`Listening on PORT: ${port}!`);
+console.log(`Listening on PORT: ${port}! Try localhost:${post}/api/v1!`);
