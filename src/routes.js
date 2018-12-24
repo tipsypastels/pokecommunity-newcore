e30c08aee1aa88a72e083f481f83036d22eeaba3
tipsypastels <dakotasankey@gmail.com>
2018-12-23 21:46:45 -0800

    deleted routes.js

--- src/routes.js
+++
@@ -1,7 +0,0 @@
-import pcRouteBuilder from './pcRouteBuilder';
-
-import UsersController from './controllers/UsersController';
-
-new pcRouteBuilder(router).buildRoutes(function() {
-  this.get('users/:userid', UsersController.show);
-});
